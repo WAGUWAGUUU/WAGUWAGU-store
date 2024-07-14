5fcf79aa9fc8a71224235697778e7533a8585d11
Kim Sehyun <kkshyun56@gmail.com>
2024-07-14 18:13:15 +0900

    [feat] : store-delivery-info

--- src/main/java/com/example/store/global/repository/StoreDeliveryInfoRepository.java
+++ src/main/java/com/example/store/global/repository/StoreDeliveryInfoRepository.java
@@ -3,5 +3,10 @@ package com.example.store.global.repository;
 import com.example.store.global.entity.StoreDeliveryInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StoreDeliveryInfoRepository extends JpaRepository<StoreDeliveryInfo, Long> {
+    Optional<StoreDeliveryInfo> findByStore_StoreIdAndStoreDeliveryInfoState(Long storeId, int storeDeliveryInfoState);
+    List<StoreDeliveryInfo> findAllByStore_StoreId(Long storeId);
 }
--- src/main/java/com/example/store/service/StoreDeliveryInfoServiceImpl.java
+++ src/main/java/com/example/store/service/StoreDeliveryInfoServiceImpl.java
@@ -1,8 +1,46 @@
 package com.example.store.service;
+import com.example.store.dto.request.StoreDeliveryInfoRequestDto;
+import com.example.store.dto.request.UpdateStoreDeliveryInfoRequestDto;
+import com.example.store.dto.response.StoreDeliveryInfoResponseDto;
+import com.example.store.global.entity.Store;
+import com.example.store.global.entity.StoreDeliveryInfo;
+import com.example.store.global.repository.StoreDeliveryInfoRepository;
+import com.example.store.global.repository.StoreRepository;
+import com.example.store.global.type.UpdateStoreDeliveryInfoType;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class StoreDeliveryInfoServiceImpl {
+public class StoreDeliveryInfoServiceImpl implements StoreDeliveryInfoService {
+    private final StoreDeliveryInfoRepository storeDeliveryInfoRepository;
+    private final StoreRepository storeRepository;
+    @Override
+    @Transactional
+    public void createStoreDeliveryInfo(Long storeId, StoreDeliveryInfoRequestDto storeDeliveryInfoRequestDto) {
+        Optional<StoreDeliveryInfo> byStoreStoreIdAndStoreDeliveryInfoState = storeDeliveryInfoRepository.findByStore_StoreIdAndStoreDeliveryInfoState(storeId, storeDeliveryInfoRequestDto.storeDeliveryInfoState());
+        if(byStoreStoreIdAndStoreDeliveryInfoState.isPresent()) throw new IllegalArgumentException();
+        Optional<Store> byId = storeRepository.findById(storeId);
+        if(byId.isEmpty()) throw new IllegalArgumentException();
+        StoreDeliveryInfo entity = storeDeliveryInfoRequestDto.toEntity(byId.get());
+        storeDeliveryInfoRepository.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public List<StoreDeliveryInfoResponseDto> getStoreDeliveryInfoAllByStoreId(Long storeId) {
+        List<StoreDeliveryInfo> allByStoreStoreId = storeDeliveryInfoRepository.findAllByStore_StoreId(storeId);
+        return allByStoreStoreId.stream().map(StoreDeliveryInfoResponseDto::from).toList();
+    }
+
+    @Override
+    @Transactional
+    public void updateStoreDeliveryInfoByStoreIdAndState(Long storeId, int storeDeliveryInfoState, UpdateStoreDeliveryInfoType updateStoreDeliveryInfoType, UpdateStoreDeliveryInfoRequestDto updateStoreDeliveryInfoRequestDto) {
+        StoreDeliveryInfo storeDeliveryInfo = storeDeliveryInfoRepository.findByStore_StoreIdAndStoreDeliveryInfoState(storeId, storeDeliveryInfoState).orElseThrow();
+        storeDeliveryInfo.update(updateStoreDeliveryInfoType, updateStoreDeliveryInfoRequestDto);
+    }
 }
